2de6ee407ea9337c4a02d83e0e310b3765f1480c
IgorKano <141119243+IgorKano@users.noreply.github.com>
2024-05-15 01:54:41 +0600

    Xnj eujlyj

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -1,22 +1,31 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main extends ApplicationAdapter {
 	public static final float SCR_HEIGHT = 900, SCR_WIDTH = 1600;
+	public static final int TURRETT_COUNT = 4;
 	SpriteBatch batch;
 	Texture img;
 	Texture imgTurret;
 	OrthographicCamera camera;
 
-	public static Enemy[] enemy = new Enemy[250];
-	Turret[] turret = new Turret[4];
-	
+	public static Enemy[] enemy = new Enemy[5];
+	Turret[] turret = new Turret[TURRETT_COUNT];
+
+	ArrayList<Bullet> bullets;
+
+	//private float fireDelay;
+	private float[] turretFireDelay = new float[TURRETT_COUNT];
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -36,27 +45,88 @@ public class Main extends ApplicationAdapter {
 		for (int i = 0; i < turret.length; i++) {
 			turret[i].spawn(i);
 		}
+
+
+		bullets = new ArrayList<Bullet>();
+
+
+
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 1, 1, 1);
+
+		float dt = Gdx.graphics.getDeltaTime();
+
+		for (int i = 0; i < turretFireDelay.length; i++){
+			turretFireDelay[i] -= dt;
+		}
+
+
+
 		batch.setProjectionMatrix(camera.combined);
 		for (int i = 0; i < enemy.length; i++) {
 			enemy[i].moveToBase(enemy[i].vx, enemy[i].vy, enemy[i].x, enemy[i].y);
 		}
 		for (int i = 0; i < turret.length; i++) {
-			turret[i].lookToEnemy(turret[i].getINearestEnemy());
+
+			int indexEnemy = turret[i].getINearestEnemy();
+			turret[i].lookToEnemy(indexEnemy);
+
+
+
+//почему то стреляет только первая туррель - самая левая которая хм...теперь две 1 и 3, 2 и 4 патроной не завезли )))
+			if (turret[i].fire){
+				if (turretFireDelay[i] <= 0){
+					bullets.add(new Bullet(turret[i].turX + (turret[i].height/2), turret[i].turY + (turret[i].height/2), turret[i].eX, turret[i].eY));
+					//bullets.add(new Bullet(turret[i].barrelX, turret[i].barrelY, turret[i].eX, turret[i].eY));
+					turretFireDelay[i] += 0.2;
+				}
+			} else {
+				turretFireDelay[i] = 0;
+			}
+
+
+
 		}
+
+
+
+		for (Bullet bullet : bullets){
+			bullet.Update(dt);
+		}
+		//Текущий размер экрана можно получить так
+		//Gdx.graphics.getWidth();
+		//Gdx.graphics.getHeight();
+		for (Iterator<Bullet> iterator = bullets.iterator(); iterator.hasNext(); ) {
+			Bullet bullet = iterator.next();
+			if (bullet.position.x <= 0 | bullet.position.x >= SCR_WIDTH | bullet.position.y <= 0 | bullet.position.y >= SCR_HEIGHT){
+				iterator.remove();
+			}
+		}
+
 		batch.begin();
 
 		for (int i = 0; i < enemy.length; i++) {
 			batch.draw(img, enemy[i].x, enemy[i].y, enemy[i].width/2, enemy[i].height/2, enemy[i].width, enemy[i].height,
 			1, 1, enemy[i].rotation, 0, 0, 256, 256, false, false);
 		}
-		for (int i = 0; i < turret.length; i++) {
-			batch.draw(imgTurret, turret[i].turX, turret[i].turY, 16, 16, turret[i].width, turret[i].height, 1, 1, turret[i].a, 0, 0, 48, 32, false, false);
+
+		for (Bullet bullet : bullets){
+			batch.draw(bullet.texture, bullet.position.x, bullet.position.y);
 		}
+
+		for (int i = 0; i < turret.length; i++) {
+			batch.draw(imgTurret, turret[i].turX, turret[i].turY, turret[i].height/2, turret[i].height/2, turret[i].width, turret[i].height, 1, 1, turret[i].a, 0, 0, 48, 32, false, false);
+		}
+
+
+
+
+
+
+
 		batch.end();
 	}
 	
@@ -64,5 +134,10 @@ public class Main extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		imgTurret.dispose();
+
+		for (Bullet bullet : bullets){
+			bullet.dispose();
+		}
 	}
 }
--- core/src/com/mygdx/game/Turret.java
+++ core/src/com/mygdx/game/Turret.java
@@ -13,9 +13,14 @@ public class Turret {
     float eY;
     float width = 48, height = 32;
 
+    float barrelX, barrelY;
+    float burrelLenghr = width - height/2;
+    boolean fire;
+
     float spaceToNearestEnemy = 0;
 
     void spawn(int turretNumber) {
+        fire = false;
         switch (turretNumber) {
             case 0: turX = (int) Main.SCR_WIDTH/5;
             turY = (int) Main.SCR_HEIGHT-100;
@@ -30,6 +35,9 @@ public class Turret {
             turY = (int) Main.SCR_HEIGHT-250;
             break;
         }
+
+        barrelX = turX + this.width;
+        barrelY = turY + this.height/2;
     }
 
     int getINearestEnemy() {
@@ -37,28 +45,48 @@ public class Turret {
         double curJ;
         int nearestEnemyNumber = 0;
         for (int i = 0; i < Main.enemy.length; i++) {
-            /*eX = Main.enemy[i].x;
-            eY = Main.enemy[i].y;*/
+
             curJ = Math.sqrt(Math.pow((double) (turX-Main.enemy[i].x), 2)+Math.pow((double) (turY-Main.enemy[i].y), 2));
             if (j > curJ) {
                 j = curJ;
                 nearestEnemyNumber = i;
             }
         }
+        eX = Main.enemy[nearestEnemyNumber].x + Main.enemy[nearestEnemyNumber].width; //они нам нужны для направления новых пуль
+        eY = Main.enemy[nearestEnemyNumber].y + Main.enemy[nearestEnemyNumber].height;
         return nearestEnemyNumber;
     }
 
     void lookToEnemy(int iNearestEnemy) {
         /*eX = Main.enemy[iNearestEnemy].x;
         eY = Main.enemy[iNearestEnemy].y;*/
-        float rotation = MathUtils.atan((turX-Main.enemy[iNearestEnemy].x)/(Main.enemy[iNearestEnemy].y-turY))*MathUtils.radiansToDegrees;
+        fire = false;
+        float rotation = MathUtils.atan((Main.enemy[iNearestEnemy].width + turX-Main.enemy[iNearestEnemy].x)/(Main.enemy[iNearestEnemy].height + Main.enemy[iNearestEnemy].y-turY))*MathUtils.radiansToDegrees;
         if (rotation > curA) {
             curA += 0.5;
+
         } else if(rotation < curA) {
             curA -= 0.5;
+
         } else if (abs(rotation - curA) < 2){
             curA = rotation;
+
         }
         this.a = curA-90;
+
+        //длина от центра вращения до дульного среза
+
+        /*barrelX = turX + this.width + (float)(burrelLenghr*Math.sin(Math.toRadians(curA)));
+        barrelY = turY + this.height/2 + (float)(burrelLenghr*Math.cos(Math.toRadians(curA)));*/
+
+        /*barrelX = turX + (float)(burrelLenghr*Math.sin(Math.toRadians(curA)));
+        barrelY = turY + (float)(burrelLenghr*Math.cos(Math.toRadians(curA)));*/
+
+
+
+        if (Math.abs(90 + this.a - rotation) < 0.5){
+            fire = true;
+        }
+
     }
 }
